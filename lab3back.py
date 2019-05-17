e627337cc3e6adb124a9400e3303c707e19e7922
JakinHuangWang <31380172+JakinHuangWang@users.noreply.github.com>
2019-05-17 12:11:06 -0700

    Final Update

--- lab3back.py
+++ lab3back.py
@@ -1,9 +1,9 @@
-"""LAB 2
+"""LAB 3 Back
 Author: Jakin Wang
 OS: Mac OSX
 IDE: Wings IDE
-Date: 05/04/2019
-Description: This Python File is used to Scrape the web and create a json file and then use that json file to create a database
+Date: 05/17/2019
+Description: This file scrapes the web, creates a json file, and uses the json file to construct a database
 """
 import urllib.request as ur
 import requests
@@ -14,32 +14,43 @@ import collections
 import json
 import sqlite3
 
-#page = requests.get("https://www.bls.gov/ooh/computer-and-information-technology/home.htm")
-#soup = BeautifulSoup(page.content, "lxml")
-#lstOfLinks = [link["href"] for link in soup.select("h4 a")]
-#titleContentdd = {}
+#For checking if a string is a valid number, including the negative sign
+def is_number(n):
+    try:
+        int(n)
+    except ValueError:
+        return False
+    return True
 
-#for link in lstOfLinks:
-    #page = requests.get("https://www.bls.gov" + link)
-    #soup = BeautifulSoup(page.content, "lxml")
-    #titleLst = soup.select("tbody tr th a")
-    #contentLst = soup.find_all("td")    
-    #titleContentdd[soup.find_all("h1")[0].text.strip()] = {titleLst[i].text.strip() :
-                                                           #contentLst[i].text.strip().split("  ")[0].split("%")[0].strip("$").replace(",", "") 
-                                                           #for i in range(7)}
-#for v in titleContentdd.values():
-    #for i, j in v.items():
-        #if j.isdigit():
-            #v[i] = int(j)
-#with open('dataLab3.json', 'w', True) as fh:
-    #json.dump(titleContentdd, fh, indent=3)
+#This portion is to create a json file by scraping the web
+'''
+page = requests.get("https://www.bls.gov/ooh/computer-and-information-technology/home.htm")
+soup = BeautifulSoup(page.content, "lxml")
+lstOfLinks = [link["href"] for link in soup.select("h4 a")]
+titleContentdd = {}
 
-conn = sqlite3.connect("Lab3.db")
-cur = conn.cursor()
+for link in lstOfLinks:
+    page = requests.get("https://www.bls.gov" + link)
+    soup = BeautifulSoup(page.content, "lxml")
+    titleLst = soup.select("tbody tr th a")
+    contentLst = soup.find_all("td")    
+    titleContentdd[soup.find_all("h1")[0].text.strip()] = {titleLst[i].text.strip() : contentLst[i].text.strip().split("  ")[0].split("%")[0].strip("$").replace(",", "")
+                                                           for i in range(7)}
+for v in titleContentdd.values():
+    for i, j in v.items():
+        if is_number(j):v[i] = int(j)
+        if j == 'See How to Become One':v[i] = 'Certificate'
+        
+with open('dataLab3.json', 'w', True) as fh:
+    json.dump(titleContentdd, fh, indent=3)
+'''
 
+#This portion is to create a database based on the json file we have created
 with open('dataLab3.json', 'r') as fh:
     datadd = json.load(fh)
-
+    
+conn = sqlite3.connect("Lab3.db")
+cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS FIELDNAMES")      
 cur.execute('''CREATE TABLE FIELDNAMES(
                ID INTEGER NOT NULL PRIMARY KEY UNIQUE,
@@ -51,9 +62,9 @@ cur.execute('''CREATE TABLE FIELDNAMES(
                C6 TEXT,
                C7 TEXT)''')
 cur.execute('''INSERT INTO FIELDNAMES (C1, C2, C3, C4, C5, C6, C7)
-               VALUES (?, ?, ?, ?, ?, ?, ?)''',tuple(datadd["Computer and Information Research Scientists"].keys()))
+               VALUES (?, ?, ?, ?, ?, ?, ?)''', tuple(datadd["Computer and Information Research Scientists"].keys()))
 cur.execute("DROP TABLE IF EXISTS DATA")      
-cur.execute("""
+cur.execute('''
             CREATE TABLE DATA(
             ID INTEGER NOT NULL PRIMARY KEY UNIQUE, 
             Name TEXT UNIQUE ON CONFLICT IGNORE,
@@ -63,12 +74,12 @@ cur.execute("""
             Training TEXT,
             Jobs INTEGER,
             Outlook INTEGER,
-            Change INTEGER )""")
+            Change INTEGER )''')
 cur.execute("DROP TABLE IF EXISTS EDUCATION")
-cur.execute("""
+cur.execute('''
             CREATE TABLE EDUCATION(
             ID INTEGER NOT NULL PRIMARY KEY UNIQUE,
-            NAME TEXT UNIQUE ON CONFLICT IGNORE)""")
+            NAME TEXT UNIQUE ON CONFLICT IGNORE)''')
 
 for name, d in datadd.items():
     v = tuple(d.values())
--- lab3front.py
+++ lab3front.py
@@ -1,8 +1,9 @@
-"""LAB 2Author: Jakin Wang
+"""LAB 3 Front
+Author: Jakin Wang
 OS: Mac OSX
 IDE: Wings IDE
-Date: 05/04/2019
-Description: Scrape the Web, Create a database, and using the database to perform data analysis and data plotting
+Date: 05/17/2019
+Description: Uses the database we create to construct a set of GUIs for the user the access
 """
 import numpy as np
 import matplotlib
@@ -14,11 +15,35 @@ import sqlite3
 
 conn = sqlite3.connect("Lab3.db")
 cur = conn.cursor()
+ 
+#Class mainWin is the Main Window for our GUI, it consists of three buttons:
+#By salary, By growth rate, and By degree
+class mainWin(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("High Tech Jobs")
+        tk.Button(self, text = "By salary" , command=lambda:plotWin(self).plot(plotTwo, "Dollars")).grid(row = 0, column = 0)
+        tk.Button(self, text = "By growth rate", command=lambda:plotWin(self).plot(plotTwo, "Percentage")).grid(row = 0, column = 1)
+        tk.Button(self, text = "By degree", command=lambda:dialogueWin(self).create(self)).grid(row = 0, column = 2)
+        
+#Class plotWin is the plotting Window for our GUI, it performs both plottings
+class plotWin(tk.Toplevel):
+    def __init__(self, master):
+        super().__init__(master)
+        self.title("Plotting For High Tech Jobs")
+        self.fig = plt.figure(figsize = (8,8))
+        self.grab_set()
+        self.focus_set()        
+    def plot(self, someFunc, *args,  **kwargs):
+        someFunc(*args, **kwargs)
+        canvas = FigureCanvasTkAgg(self.fig, master=self)
+        canvas.get_tk_widget().grid()
+        canvas.draw()
 
 #Function plotTwo plots two similar graphs: 2018 Median Pay and Job Outlook 2016-26
 def plotTwo(label):
     if label == 'Dollars':
-        plt.title(cur.execute("SELECT {} FROM FIELDNAMES".format("C1")))
+        cur.execute("SELECT {} FROM FIELDNAMES".format("C1"))
         plt.title(cur.fetchone()[0])
         cur.execute("SELECT Name, MedianPay FROM DATA ORDER BY MedianPay")
     else:
@@ -28,55 +53,38 @@ def plotTwo(label):
     result = cur.fetchall()
     xlst = [r[0] for r in result]
     ylst = [r[1] for r in result]
-    plt.barh(np.arange(10), ylst, align = "center")    
-    plt.yticks(np.arange(10), ['\n'.join(x.split()) for x in xlst], fontsize = 8)
+    plt.barh(np.arange(10), ylst, align = "center")
+    plt.yticks(np.arange(10), xlst, wrap=True, fontsize=6, verticalalignment='center')
     plt.xlabel(label)
- 
-#Class mainWin is the Main Window for our GUI, it consists of three buttons:
-#By salary, By growth rate, and By degree
-class mainWin(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        tk.Button(self, text = "By salary" , command=lambda:plotWin(self).plot(plotTwo, "Dollars")).grid(row = 0, column = 0)
-        tk.Button(self, text = "By growth rate", command=lambda:plotWin(self).plot(plotTwo, "Percentage")).grid(row = 0, column = 1)
-        tk.Button(self, text = "By degree", command=lambda:dialogueWin(self).create()).grid(row = 0, column = 2)
-
-#Class plotWin is the plotting Window for our GUI, it performs both plottings
-class plotWin(tk.Toplevel):
-    def __init__(self, master):
-        super().__init__(master)
-        self.title("Plotting Window")
-        self.fig = plt.figure(figsize = (10, 10))
-        self.grab_set()
-        self.focus_set()        
-    def plot(self, someFunc, *args,  **kwargs):
-        someFunc(*args, **kwargs)
-        canvas = FigureCanvasTkAgg(self.fig, master=self)
-        canvas.get_tk_widget().grid()
-        canvas.draw()
 
 #Class dialogueWin is the dialogue Window that gives the users 4 Entry-Level Education to choose from
 class dialogueWin(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
-        self.title("Dialogue Window")        
-    def create(self):
+        self.title("Choose Entry Degree")
+        self.grab_set()
+        self.focus_set()
+    def create(self, master):
         cur.execute("SELECT NAME FROM EDUCATION")
         result = cur.fetchall()
         textLst = [r[0] for r in result]
         controlVar = tk.IntVar()
-        rbLst = [tk.Radiobutton(self, text=textLst[i], variable=controlVar, value=i+1, command = lambda:displayWin(self).show(controlVar.get())) for i in range(len(textLst))]
+        rbLst = [tk.Radiobutton(self, text=textLst[i], variable=controlVar, value=i+1) 
+                 for i in range(len(textLst))]
         for RB in rbLst:
-            RB.grid()
+            RB.grid(sticky = 'w')
+        tk.Button(self, text="OK", command = lambda:displayWin(master).show(controlVar.get(), self)).grid()
             
 #After the user has chosen an option from the dialogue Window,
 #Class displayWin displays a listbox including all the jobs with the same Entry-Level Educations
 class displayWin(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
-        self.title("Display Window")
         self.focus_set()
-    def show(self, number):
+    def show(self, number, dWin):
+        dWin.destroy()
+        cur.execute("SELECT NAME FROM EDUCATION WHERE ID = ?", (number, ))
+        self.title("Minimum Degree: " + cur.fetchone()[0])
         cur.execute("SELECT NAME FROM DATA WHERE ENTRYLEVEL = ?", (number,))
         result = cur.fetchall()
         textLst = [r[0] for r in result]
